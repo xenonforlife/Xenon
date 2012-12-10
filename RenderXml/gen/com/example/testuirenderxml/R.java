044ae2bca00399fcceedfe85bf4222149c0e6897
xenonforlife <xenonforlife@gmail.com>
2012-12-10 17:40:54 +0100

    Implementing Multi Pane Fragments

--- RenderXml/gen/com/example/testuirenderxml/R.java
+++ RenderXml/gen/com/example/testuirenderxml/R.java
@@ -15,11 +15,18 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int mainListView=0x7f070000;
-        public static final int menu_settings=0x7f070001;
+        public static final int browse_menu_fragment=0x7f070001;
+        public static final int editText1=0x7f070004;
+        public static final int editText2=0x7f070005;
+        public static final int fragment_container=0x7f070000;
+        public static final int mainListView=0x7f070003;
+        public static final int menu_settings=0x7f070006;
+        public static final int rendered_fragment=0x7f070002;
     }
     public static final class layout {
         public static final int activity_render_xml=0x7f030000;
+        public static final int menu_fragment_layout=0x7f030001;
+        public static final int test=0x7f030002;
     }
     public static final class menu {
         public static final int activity_render_xml=0x7f060000;
--- RenderXml/src/com/example/testuirenderxml/RenderXml.java
+++ RenderXml/src/com/example/testuirenderxml/RenderXml.java
@@ -1,124 +1,43 @@
 package com.example.testuirenderxml;
 
-import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.fdi._2010.client.ActionT;
-import org.fdi._2010.client.ChartT;
-import org.fdi._2010.client.DialogT;
-import org.fdi._2010.client.EnumerationItemListT;
-import org.fdi._2010.client.EnumerationItemT;
-import org.fdi._2010.client.GraphT;
-import org.fdi._2010.client.GridT;
-import org.fdi._2010.client.GroupT;
-import org.fdi._2010.client.HandlingT;
-import org.fdi._2010.client.ImageT;
-import org.fdi._2010.client.LayoutItemsT;
-import org.fdi._2010.client.LayoutObjectT;
-import org.fdi._2010.client.LayoutT;
-import org.fdi._2010.client.PageT;
-import org.fdi._2010.client.ParameterT;
-import org.fdi._2010.client.PluginT;
-import org.fdi._2010.client.TableT;
-import org.fdi._2010.client.UiTemplateT;
-import org.fdi._2010.client.UidLayoutInformation;
-import org.fdi._2010.client.WindowT;
-import org.xml.sax.InputSource;
 
-import ae.com.sun.xml.bind.v2.model.annotation.RuntimeInlineAnnotationReader;
-import ae.com.sun.xml.bind.v2.model.annotation.XmlSchemaMine;
-import ae.javax.xml.bind.JAXBContext;
-import ae.javax.xml.bind.JAXBElement;
-import ae.javax.xml.bind.JAXBException;
-import ae.javax.xml.bind.Unmarshaller;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 public class RenderXml extends FragmentActivity
 {
 
-	private static String choice;
-	private static final String MENU="class org.fdi._2010.client.MenuT";
-	private static final String WINDOW="class org.fdi._2010.client.WindowT";
-	private static final String DIALOG="class org.fdi._2010.client.DialogT";
-	private static final String PAGE="class org.fdi._2010.client.PageT";
-	private static final String GROUP="class org.fdi._2010.client.GroupT";
-	private static final String PARAMETER="class org.fdi._2010.client.ParameterT";
-	private static final String ACTION="class org.fdi._2010.client.ActionT";
-	private static final String TABLE="class org.fdi._2010.client.TableT";
-	private static final String CHART="class org.fdi._2010.client.ChartT";
-	private static final String GRAPH="class org.fdi._2010.client.GraphT";
-	private static final String IMAGE="class org.fdi._2010.client.ImageT";
-	private static final String TEXT="class org.fdi._2010.client.TextT";
-	private static final String PLUGIN="class org.fdi._2010.client.PluginT";
-	private static final String EDIT_DISPLAY="class org.fdi._2010.client.EditDisplayT";
-	private static final String ROW_BREAK="RowBreak";
-	private static final String COLUMN_BREAK="ColumnBreak";
-	private static final String SEPARATOR="Separator";
-	private  ArrayList<String> firstMenu=new ArrayList<String>();
-	private  ArrayList<String> nextMenu=new ArrayList<String>();	
-	private  List<Object> myList;
-	private  ArrayAdapter<String> firstAdapter;
-	private  ArrayAdapter<String> nextAdapter;
-	private  ListView listView;
-	private static boolean isfirstList=true;
-	private LayoutT browseObject;
-	private List<JAXBElement<?>> items;
-	private JAXBContext jc;
-	private Unmarshaller unmarshaller;
-	
-	public void generateFirstMenu() throws JAXBException
-	{
-	    jc = JAXBContext.newInstance("org.fdi._2010.client");
-		unmarshaller = jc.createUnmarshaller();
-		
-		InputSource xmlString = new InputSource(new StringReader(XmlString.xmlString));
-		UidLayoutInformation myLayout=(UidLayoutInformation)unmarshaller.unmarshal(xmlString);
-		RuntimeInlineAnnotationReader.cachePackageAnnotation(LayoutT.class.getPackage(), new XmlSchemaMine("http://fdi.org/2010/client"));
-		
-		myList=myLayout.getMenuOrPageOrGroup();
-	
-		if(!firstMenu.isEmpty())
-			 firstMenu.clear();
-		for(int i=0;i<myList.size();i++)
-		{
-			firstMenu.add(((LayoutObjectT)(myList.get(i))).getLabel());
-						
-		}
-		
-	    firstAdapter = new ArrayAdapter<String>(this,
-		      	 android.R.layout.simple_list_item_1, firstMenu);
-		listView.setAdapter(firstAdapter);
-	
-		listView.setOnItemClickListener(myListener);
-		
-	
-	}
-	
-	
+	/** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-    	
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_render_xml);
-		listView = (ListView) findViewById(R.id.mainListView);
-		RuntimeInlineAnnotationReader.cachePackageAnnotation(WindowT.class.getPackage(), new XmlSchemaMine("http://fdi.org/2010/client"));
-        try {
-			generateFirstMenu();
-		}  
-        catch (JAXBException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-        
+
+        // Check whether the activity is using the layout version with
+        // the fragment_container FrameLayout. If so, we must add the first fragment
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create an instance of ExampleFragment
+            BrowseMenuFragment firstFragment = new BrowseMenuFragment();
+
+            // In case this activity was started with special instructions from an Intent,
+            // pass the Intent's extras to the fragment as arguments
+            firstFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, firstFragment).commit();
+            
+         }
     }
 
     @Override
@@ -127,249 +46,6 @@ public class RenderXml extends FragmentActivity
         return true;
     }
     
-    void browse(LayoutT browseObj)
-	{
-    	browseObject=browseObj;
-    	LayoutItemsT itemList;
-		itemList=browseObj.getItemList();
-		items=itemList.getWindowOrDialogOrPage();
-		System.out.println("Please select one of the following :-");
-		if(!nextMenu.isEmpty())
-			nextMenu.clear();
-		for(int i=0;i<items.size();i++)
-		{	
-			nextMenu.add(((LayoutT)(items.get(i).getValue())).getLabel());			
-		}
-	
-		if(nextAdapter==null)
-		{
-			nextAdapter = new ArrayAdapter<String>(this,
-			      	 android.R.layout.simple_list_item_1, nextMenu);
-			listView.setAdapter(nextAdapter);
-		}
-		nextAdapter.notifyDataSetChanged();
-		
-	}
-    
-    OnItemClickListener myListener= new OnItemClickListener() 
-	{
-	    @Override
-	    public void onItemClick(AdapterView<?> adapterView, View arg1, int position, long arg3) 
-	    {
-	    	
-	    	if(isfirstList)
-	    	{
-	    		LayoutT browseMe=(LayoutT)(myList.get(position));
-	    		browseMe.parentNode=null;
-				isfirstList=false;
-				browse(browseMe);
-	    	}
-	    	else
-	    	{	
-	    		JAXBElement<?> clickedElement=items.get(position);
-				String elementType= clickedElement.getDeclaredType().toString();
-				if(elementType.contentEquals(MENU))
-				{
-					LayoutT browseChild=(LayoutT)(clickedElement.getValue());
-					if(browseChild.parentNode==null)
-					{
-						browseChild.parentNode=browseObject;
-					}
-					browse(browseChild);
-				}
-				else 
-				{
-					if(elementType.contentEquals(GROUP))
-					{
-						System.out.println("RENDERABLE!! I am a "+elementType);
-						GroupT renderMe = (GroupT)(clickedElement.getValue());
-						renderGroup(renderMe);
-					}
-					else if(elementType.contentEquals(PAGE))
-					{
-						System.out.println("RENDERABLE!! I am a "+elementType);
-						PageT renderMe = (PageT)(clickedElement.getValue());
-						renderPage(renderMe);
-					}
-					else if(elementType.contentEquals(WINDOW))
-					{
-						System.out.println("RENDERABLE!! I am a "+elementType);
-						WindowT renderMe = (WindowT)(clickedElement.getValue());
-						renderWindow(renderMe);
-					}
-					else if(elementType.contentEquals(DIALOG))
-					{
-						System.out.println("RENDERABLE!! I am a "+elementType);
-						DialogT renderMe = (DialogT)(clickedElement.getValue());
-						renderDialog(renderMe);
-					}
-					System.out.println("Cannot browse deeper, Enter 0 and go back");
-				}
-	    	}
-	   	}	
-	    
-	};
-	
-	
-	
-
-	private static void renderGroup(GroupT group)
-	{
-		System.out.println("\n	Rendering Group : "+ group.getLabel());
-		GroupT.ItemList groupItemList=group.getItemList();
-		List<JAXBElement<?>> groupItems=groupItemList.getWindowOrDialogOrPage();
-		callAptRenderMethod(groupItems);
-	}
-	
-	private static void renderPage(PageT page)
-	{
-		System.out.println("\n	Rendering Page : "+page.getLabel()+" in a new tab!");
-		PageT.ItemList pageItemList=page.getItemList();
-		List<JAXBElement<?>> pageItems=pageItemList.getWindowOrDialogOrPage();
-		callAptRenderMethod(pageItems);
-	}
-	
-	private static void renderWindow(WindowT window)
-	{
-		System.out.println("\n	Rendering Window : "+window.getLabel());
-		WindowT.ItemList windowItemList=window.getItemList();
-		List<JAXBElement<?>> windowItems=windowItemList.getWindowOrDialogOrPage();
-		callAptRenderMethod(windowItems);
-	}
-
-	private static void renderDialog(DialogT dialog)
-	{
-		System.out.println("\n	Rendering Dialog : "+dialog.getLabel());
-		DialogT.ItemList dialogItemList=dialog.getItemList();
-		List<JAXBElement<?>> dialogItems=dialogItemList.getWindowOrDialogOrPage();
-		callAptRenderMethod(dialogItems);
-		
-	}
-	
-	private static void callAptRenderMethod(List<JAXBElement<?>> items)
-	{
-		JAXBElement<?> item;
-		String itemType;
-		ParameterT parameter;
-		ActionT action;
-		WindowT window;
-		PageT page;
-		GroupT group;
-		DialogT dialog;
-		TableT table;
-		ChartT chart;
-		GraphT graph;
-		GridT grid;
-		ImageT image;
-		PluginT plugin;
-		
-		for(int i=0;i<items.size();i++)
-		{
-			item=items.get(i);
-			itemType=item.getDeclaredType().toString();
-			if(itemType.contentEquals(PARAMETER))
-			{
-				parameter=(ParameterT)item.getValue();
-				renderParam(parameter);				
-			}
-			else if(itemType.contentEquals(ACTION))
-			{
-				action=(ActionT)item.getValue();
-				renderButton(action);
-			}
-			else if(itemType.contentEquals(WINDOW))
-			{
-				window=(WindowT)item.getValue();
-				renderWindow(window);
-			}
-			else if(itemType.contentEquals(PAGE))
-			{
-				page=(PageT)item.getValue();
-				renderPage(page);
-			}
-			else if(itemType.contentEquals(DIALOG))
-			{
-				dialog=(DialogT)item.getValue();
-				renderDialog(dialog);
-			}
-			else if(itemType.contentEquals(GROUP))
-			{
-				group=(GroupT)item.getValue();
-				renderGroup(group);
-			}
-			else if(itemType.contentEquals(GRAPH))
-			{
-				graph=(GraphT)item.getValue();
-				renderGraph(graph);
-			}
-			else if(itemType.contentEquals(CHART))
-			{
-				chart=(ChartT)item.getValue();
-				renderChart(chart);
-			}
-			else if(itemType.contentEquals(TABLE))
-			{
-				table=(TableT)item.getValue();
-				renderTable(table);
-			}
-			else if(itemType.contentEquals(IMAGE))
-			{
-				image=(ImageT)item.getValue();
-				renderImage(image);
-			}
-			
-		}
-	}
-	
-	private static void renderParam(ParameterT parameter)
-	{
-		String paramName = parameter.getLabel();
-		UiTemplateT uiTemplate=parameter.getUITemplate();
-		HandlingT handling= parameter.getHandling();
-		if(uiTemplate==null)
-		{
-			System.out.println("A simple EditText : "+paramName);
-		}
-		else
-		{
-			System.out.println("A simple DropDown List : "+paramName);
-			EnumerationItemListT enumList=uiTemplate.getEnumeration();
-			List<EnumerationItemT> enumItems=enumList.getEnumerationItem();
-			for(int i=0;i<enumItems.size();i++)
-			{
-				
-			}
-		}
-	}
-	
-	private static void renderButton(ActionT action)
-	{
-		System.out.println("Button : "+action.getName());
-	}
-	
-	private static void renderGraph(GraphT graph)
-	{
-		System.out.println("Graph : "+graph.getLabel());
-	}
-	
-	private static void renderChart(ChartT chart)
-	{
-		System.out.println("Chart : "+chart.getLabel());
-	}
-	
-	private static void renderImage(ImageT image)
-	{
-		System.out.println("Image : "+image.getLabel());
-	}
-	
-	private static void renderGrid(GridT grid)
-	{
-		System.out.println("Grid");
-	}
-	
-	private static void renderTable(TableT table)
-	{
-		System.out.println("Table : "+table.getLabel());
-	}
+   
 	
 }
